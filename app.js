c499de734653edaf6e7840b320ab82df48daa31e
AbisaiSan <abisaiweb7@gmail.com>
2020-09-16 16:59:25 -0300

    Inicio do front-end e funções de inicio do jogo e fim do jogo

---
+++ app.js
@@ -0,0 +1,23 @@
+new Vue({
+  el:'#app',
+  data:{
+      running:false,
+      playerLife:10,
+      monsterLife:10
+  },
+  computed:{
+      hasResult(){
+        return this.playerLife == 0 || this.monsterLife == 0
+      }
+  },
+  methods:{
+      startGame() {
+        this.running = true
+        this.playerLife = 100
+        this.monsterLife = 100
+      }
+  },
+  watch:{
+
+  }
+})
